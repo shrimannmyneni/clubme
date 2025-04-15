d9573e5d871f9e97c51199dc1f91f953d35c246e
Shrimann Myneni <shrimann@umich.edu>
2025-04-15 11:18:45 -0400

    pls work

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Profile from './pages/Profile/profile';
 import Login from './pages/Login/login';
+import Dashboard from './pages/Dashboard/dashboard';
+
 import Layout from './components/layout'
 import './App.css';
 
 import Navbar from './components/Navbar/navbar'
 import Footer from './components/Footer/footer';
-import Dashboard from './pages/Dashboard/dashboard';
 
 function App() {
   return (
