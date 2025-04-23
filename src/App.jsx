c8a1728dd7ce0b918dc04a803c338728e9ad9c2b
Shrimann Myneni <shrimann@umich.edu>
2025-04-22 22:34:43 -0400

    Fix final casing for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Profile from './pages/Profile/profile';
-import Login from './pages/Login/login';
-import Dashboard from './pages/Dashboard/dashboard';
+import Profile from './pages/Profile/Profile';
+import Login from './pages/Login/Login';
+import Dashboard from './pages/Dashboard/Dashboard';
 
 import Layout from './components/layout'
 import './App.css';
 
-import Navbar from './components/Navbar/navbar'
-import Footer from './components/Footer/footer';
+import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
--- src/pages/Dashboard/dashboard.jsx
+++ src/pages/Dashboard/dashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Card from '../../components/ui/Card/card';
+import Card from '../../components/ui/Card/Card';
 
 // Sample data
 const sampleData = [
